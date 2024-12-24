77f3b08f57024ed23ef8aad21a11c82e0f14eaf2
Andrii <andrii.kondratovich@gmail.com>
2024-12-24 14:46:23 +0200

    add: comments about logger

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
-import logger from "morgan";
+import logger from "morgan"; // outputs to console request info
 import cors from "cors";
 import { contactsRouter } from "./routes/api/contactsRouter.js";
 
 export const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+// In package.json, depends of value (development or production) in variable ENV (aka NODE_ENV) will be showed full or short info
+// cross-env NODE_ENV=production nodemon server.js - will show full info
+const formatsLogger = app.get("env") === "development" ? "dev" : "short"; // read ENV and show full or short info
 
-app.use(logger(formatsLogger));
+app.use(logger(formatsLogger)); // show full or short info in log
 app.use(cors());
 app.use(express.json()); // Checks if exist body in each request. If exist, it checks type by header "Content-Type". If Content-Type === "application/json, this middleware convert it from string to object (by JSON.parse())
 
