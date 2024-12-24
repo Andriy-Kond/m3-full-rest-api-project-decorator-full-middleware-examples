e4b6f3147c542d8badff054352c489e3c779d21d
Andrii <andrii.kondratovich@gmail.com>
2024-12-24 14:37:55 +0200

    add: example

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import { app } from "./app.js";
 import mongoose from "mongoose";
 
 // require("dotenv").config();
-import "dotenv/config";
+import "dotenv/config"; // Method .config() looks for file .env, reads it and add to process.env keys with values.
 
-const port = process.env.PORT || 3000;
-const DB_HOST = process.env.DB_HOST;
+// const port = process.env.PORT || 3000;
+const { DB_HOST, PORT: port = 3000 } = process.env;
 
 // console.log("process.env.DB_HOST:::", process.env.DB_HOST);
 // console.log("process.env.PORT:::", process.env.PORT);
